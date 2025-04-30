3b2f4a5d928a791fd1e2eb4aaa5a924139af18e7
binivazqua <binizavazquez@gmail.com>
2025-04-30 12:33:02 -0600

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,35 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 function Navbar() {
+  // Creamos un estado que comienza con el valor "false".
+  // menuOpen -> estado actual
+  // setMenuOpen -> función que cambia el estado.
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="flex flex-col md:flex-row justify-between p-4 bg-purple-400 w-full">
-      <h1 className="text-white font-bold text-3xl w-full">Coldwell Banker</h1>
-      <ul className="flex flex-col md:flex-row md:ml-auto items-center space-y-2 md:space-y-0 md:space-x-8 w-full md:w-auto">
+      <div className="flex flex-row">
+        <h1 className="text-white font-bold text-3xl w-full">
+          Coldwell Banker
+        </h1>
+        {/* Botón hamburguesa */}
+        <button
+          className="bg-transparent md:hidden text-white text-3xl"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <HiX /> : <HiMenu />}
+        </button>
+      </div>
+      {/*<ul className="flex flex-col md:flex-row md:ml-auto items-center space-y-2 md:space-y-0 md:space-x-8 w-full md:w-auto"> */}
+      <ul
+        className={`flex flex-col space-y-2 md:flex-row ${
+          menuOpen ? "block" : "hidden"
+        } md:flex`}
+      >
         <li className="">
           <a
             href="/home"
-            className="block w-full bg-white md:bg-transparent text-black no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent transition text-center"
+            className="block w-full bg-white md:bg-transparent text-purple-600 no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-200 transition text-center"
           >
             Home
           </a>
@@ -16,7 +37,7 @@ function Navbar() {
         <li className="">
           <a
             href="/catalogue"
-            className="block w-full bg-white md:bg-transparent text-black no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent transition text-center"
+            className="block w-full bg-white md:bg-transparent text-purple-600 no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-200 transition text-center"
           >
             Catalogue
           </a>
@@ -24,7 +45,7 @@ function Navbar() {
         <li className="">
           <a
             href="/contact"
-            className="block w-full bg-white md:bg-transparent text-black no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent transition text-center"
+            className="block w-full bg-white md:bg-transparent text-purple-600 no-underline font-bold px-4 py-2 rounded-lg md:rounded-none hover:bg-gray-100 md:hover:bg-transparent md:hover:text-purple-200 transition text-center"
           >
             Contact
           </a>
